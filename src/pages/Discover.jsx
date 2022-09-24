96edcdb7149bec6c6129ac8bbaa2beefd38479f3
vikash sirvi <vikashsirvi9999@gmail.com>
2022-09-24 17:26:32 +0530

    song card component added

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,10 +1,14 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
+    const dispatch = useDispatch();
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
     const { data, isFetching, error } = useGetTopChartsQuery();
     const genreTitle = 'Pop';
+
 
     if (isFetching) return <Loader title="Loading songs..." />
     if (error) return <Error />
@@ -23,7 +27,14 @@ const Discover = () => {
             </div>
             <div className="flex flex-wrap sm:justify-start justify-center gap-8">
                 {
-                    data?.map((song, i) => <SongCard key={song.key} song={song} i={i} />)
+                    data?.map((song, i) =>
+                        <SongCard
+                            key={song.key}
+                            song={song} i={i}
+                            isPlaying={isPlaying}
+                            activeSong={activeSong}
+                            data={data}
+                        />)
                 }
             </div>
         </div>
