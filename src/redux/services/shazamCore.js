9e78b5892f69a45b68a2a57014a505fcaf367014
vikash sirvi <vikashsirvi9999@gmail.com>
2022-09-25 22:37:18 +0530

    search functionality added

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,7 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-console.log(import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
-
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
@@ -13,18 +11,23 @@ export const shazamCoreApi = createApi({
     }),
     endpoints: (builder) => ({
         getTopCharts: builder.query({ query: () => '/charts/world' }),
+        getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
         getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
         getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
         getArtistDetails: builder.query({ query: (artistid) => `/artists/details?artist_id=${artistid}` }),
         getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode ? countryCode : 'US'}` }),
+        getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+
     })
 
 })
 
 export const {
     useGetTopChartsQuery,
+    useGetSongsByGenreQuery,
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
     useGetArtistDetailsQuery,
-    useGetSongsByCountryQuery
+    useGetSongsByCountryQuery,
+    useGetSongsBySearchQuery,
 } = shazamCoreApi;
